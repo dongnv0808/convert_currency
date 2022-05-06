e7a9e908a7610314a025e18df4ad5744c1461986
dongnv0808 <yamaraja0808@gmail.com>
2022-05-06 10:15:40 +0700

    update

--- convert_currency.js
+++ convert_currency.js
@@ -1,24 +1,26 @@
 // let convert = document.getElementById("convert");
 // convert = document.addEventListener("click",convert_currency);
 function convert_currency() {
-    let convert;
     let amount = document.getElementById("amount").value;
     let fromCurrency = document.getElementById("fromCurrency").value;
     let toCurrency = document.getElementById("toCurrency").value;
-        if ( fromCurrency == "VND" && toCurrency == "USD")  {
-            convert = amount/23000;
-            document.write( + convert);
-        }
-        else if ( fromCurrency == "USD" && toCurrency == "VND") {
-            convert = amount*23000;
-            console.log( + convert);
-            document.write( + convert);
-        }
-        else if (toCurrency == "VND" && fromCurrency == "VND"){
-            document.write( + amount);
-        }
+    let convert = amount/fromCurrency*toCurrency;
+    document.write(+convert);
+        // if ( fromCurrency == "VND" && toCurrency == "USD")  {
+        //     convert = amount/23000;
+        //     document.write( + convert);
+        // }
+        // else if ( fromCurrency == "USD" && toCurrency == "VND") {
+        //     convert = amount*23000;
+        //     console.log( + convert);
+        //     document.write( + convert);
+        // }
+        // else if (toCurrency == "VND" && fromCurrency == "VND"){
+        //     document.write( + amount);
+        // }
         
-        else if (toCurrency == "USD" && fromCurrency == "USD"){
-            document.write( + amount);
-        }
+        // else if (toCurrency == "USD" && fromCurrency == "USD"){
+        //     document.write( + amount);
+        // }
+
     }
